64ac90ad969728cb96e224e38a0c055b297bfbf8
hjmoesta <moestahe@msu.edu>
2020-08-29 13:35:04 -0400

    added code to take todays data and display it in graph

--- data_export.py
+++ data_export.py
@@ -1,10 +1,45 @@
 import sqlite3
 import datetime
+import matplotlib.pyplot as plt
+import julian
+
 def export_data(download, upload,table,db):
    conn = sqlite3.connect(db)
    cur = conn.cursor()
    data = (download,upload)
-   string = "INSERT INTO wifi_speed VALUES(?,?,julianday('now'));"
+   string = "INSERT INTO {} VALUES(?,?,julianday('now'));".format(table)
    cur.execute(string,data)
    conn.commit()
    conn.close()
+
+def todays_data(table,db):
+   download = []
+   upload=[]
+   julian_date=[]
+   conn = sqlite3.connect(db)
+   cur = conn.cursor()
+   string = "SELECT * FROM {} WHERE date > julianday('now')-1;".format(table)
+   cur.execute(string)
+   rows = cur.fetchall()
+   for row in rows:
+      download.append(row[0])
+      upload.append(row[1])
+      julian_date.append(row[2])
+   date = julian_to_seconds(julian_date)
+   return(graph_data(download,upload,date))
+
+def graph_data(upload,download,time):
+   plt.plot(time,download,linewidth=2.0)
+   plt.plot(time,upload,linewidth=2.0)
+   plt.show()
+   #plt.savefig("todays_newtworkk_graph.pdf")
+
+def julian_to_seconds(jul):
+   date=[]
+   for i in range(len(jul)):
+      time=julian.from_jd(jul[i],fmt='mjd')
+      total_sec = time.hour*3600 + time.minute*60 + time.second 
+      date.append(total_sec)
+   for i in range(len(date)):
+      date[i]=date[i]-date[0]
+   return date
--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import data_export
 import wifi_statistics
 from email_functions import SendMessage
 import email_details
+import time
+import datetime
+
+wait_time = 600 #10 minutes
 
 def main():
-    value = 0
     db = 'wifi_stats.db'
     table = 'wifi_speed'
+    upload, download = wifi_statistics.speedtest_cli()
+    print(download,upload)
+    if download <= 10.0:
+        SendMessage(email_details.sender, email_details.to, email_details.subject, email_details.msgHtml_error + str(download), email_details.msgPlain)
+    data_export.export_data(download, upload, table, db)
+    current = datetime.datetime.now()
+    if current.hour == 12 and current.minute == 0:
+        graph = data_export.todays_data(table,db)
 
-    while value < 1:
-        upload, download = wifi_statistics.speedtest_cli()
-        print(download,upload,value)
-        if download <= 10.0:
-            SendMessage(email_details.sender, email_details.to, email_details.subject, email_details.msgHtml_error + str(download), email_details.msgPlain)
-        data_export.export_data(download, upload, table, db)
-        value += 1
+    time.sleep(wait_time)
 
     
 
